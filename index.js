c9c54a12b1153167f1fdf7fbda3ac5c44ffab89a
Pooja <poojasreegiddaluri01@gmail.com>
2023-04-04 11:05:53 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-container">
+    <div className="content-container">
+      <h1 className="heading">Super Over League</h1>
+      <div className="image-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+          className="image"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+          className="image"
+        />
+      </div>
+    </div>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
